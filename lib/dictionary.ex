1b7fbc6c8be37e33165ea25579f7758d4d7a2f5a
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-08-09 16:54:10 -0400

    Same functions with pipe symbol

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -13,8 +13,9 @@ defmodule Dictionary do
 
   """
   def word_list do
-    words = File.read!("assests/words.txt")
-    String.split(words, ~r/\n/, trim: true)
+  "assests/words.txt" |> File.read!() |> String.split(~r/\n/, trim: true)
+    # words = File.read!("assests/words.txt")
+    # String.split(words, ~r/\n/, trim: true)
   end
   def random_word do
   Enum.random(word_list())
